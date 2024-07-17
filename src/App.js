95ffd418ac419e8d8cd4220eaf6783a299ed23d7
Hirideindia <hirideindia@gmail.com>
2024-07-17 22:15:43 +0530

    8

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import BookingForm from './components/BookingForm';
-import './App.css';
+import './App.css';  // Importing the CSS file
 
 const App = () => {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<BookingForm />} />
-      </Routes>
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<BookingForm />} />
+        </Routes>
+      </div>
     </Router>
   );
 };

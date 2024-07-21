2ff26eda95b5d0a82bc85b63ef2524a35a9bce1f
Hirideindia <hirideindia@gmail.com>
2024-07-21 14:47:32 +0530

    3

--- src/App.js
+++ src/App.js
@@ -1,39 +1,14 @@
-<<<<<<< HEAD
 import React from 'react';
-import DriverList from './components/DriverList';
-import HireDriverForm from './components/HireDriverForm';
 import './App.css';
 
-const App = () => {
+function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Welcome to HiRide</h1>
+        <h1>Welcome to Hiride</h1>
       </header>
-      <DriverList />
-      <HireDriverForm />
     </div>
   );
-};
+}
 
 export default App;
-=======
-import React from 'react';
-import DriverList from './components/DriverList';
-import HireDriverForm from './components/HireDriverForm';
-import './App.css';
-
-const App = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Welcome to HiRide</h1>
-      </header>
-      <DriverList />
-      <HireDriverForm />
-    </div>
-  );
-};
-
-export default App;
->>>>>>> a88f2e85337e91751fe57a7b5d0c804af84cf635
--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
@@ -13,19 +12,3 @@ ReactDOM.render(
 );
 
 reportWebVitals();
-=======
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-reportWebVitals();
->>>>>>> a88f2e85337e91751fe57a7b5d0c804af84cf635
